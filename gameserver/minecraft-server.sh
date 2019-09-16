2a122dab5e73f3ad907768acd09f87d2a4323f28
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2019-09-16 18:19:48 +0200

    Update minecraft-server.sh

--- gameserver/minecraft-server.sh
+++ gameserver/minecraft-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install epel-release -y && yum install mailx postfix curl wget bzip2 gzip unzip python binutils bc jq tmux java-1.8.0-openjdk
+yum install epel-release -y && yum install mailx postfix curl wget bzip2 gzip unzip python binutils bc jq tmux java-1.8.0-openjdk -y
 
 adduser mcserver
 passwd mcserver
