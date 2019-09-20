2b1d264b2fe8de96f21c29ef1a6507a3630d3b19
rcapaul1 <40087283+rcapaul1@users.noreply.github.com>
2019-09-20 08:23:39 +0200

    Update ark-server-auto.sh

--- gameserver/ark-server-auto.sh
+++ gameserver/ark-server-auto.sh
@@ -5,4 +5,4 @@ passwd arkserver
 yum install epel-release -y && yum install mailx postfix curl wget bzip2 gzip unzip python binutils bc jq tmux glibc.i686 libstdc++ libstdc++.i686 -y
 cd /home/arkserver
 su arkserver -c "wget -O linuxgsm.sh https://linuxgsm.sh && chmod +x linuxgsm.sh && bash linuxgsm.sh arkserver"
-su arkserver -c "./arkserver install"
+su arkserver -c "./arkserver ai"
